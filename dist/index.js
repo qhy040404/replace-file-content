529a35fe17f96d3c4d17d348fed37108da176599
qhy040404 <qhy040404@163.com>
2022-08-19 09:08:43 +0800

    dist

--- dist/index.js
+++ dist/index.js
@@ -43,7 +43,7 @@ exports.findFiles = void 0;
 const glob = __importStar(__nccwpck_require__(2760));
 function findFiles(name) {
     return __awaiter(this, void 0, void 0, function* () {
-        const globber = yield glob.create(`**/${name}`);
+        const globber = yield glob.create(`${name}`);
         const files = yield globber.glob();
         return files[0];
     });
@@ -102,29 +102,13 @@ function run() {
         const new_data = core.getInput('content', { required: true });
         const count = parseInt(core.getInput('count'));
         let timer = 0;
-        /**
-         * debug
-         */
-        core.info(file_name);
-        core.info(past_data);
-        core.info(new_data);
-        core.info(String(count));
-        core.info(String(timer));
         const file = yield (0, file_helper_1.findFiles)(file_name);
         let data = fs.readFileSync(file, 'utf8');
         while (timer < count) {
             timer++;
-            /**
-             * debug
-             */
-            core.info(`timer${timer}`);
             data = data.replace(past_data, new_data);
         }
         try {
-            /**
-             * debug
-             */
-            core.info(data);
             fs.writeFileSync(file, data, 'utf8');
             core.info('File overwritten');
         }
