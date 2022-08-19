03ebd85fd7a9b508196ef0ce077a9950e4524534
qhy040404 <qhy040404@163.com>
2022-08-19 09:08:29 +0800

    Remove debugger and filehelper

--- src/main.ts
+++ src/main.ts
@@ -10,33 +10,15 @@ async function run() {
     const count = parseInt(core.getInput('count'))
     let timer = 0
 
-    /**
-     * debug
-     */
-    core.info(file_name)
-    core.info(past_data)
-    core.info(new_data)
-    core.info(String(count))
-    core.info(String(timer))
-
     const file = await findFiles(file_name)
 
     let data = fs.readFileSync(file, 'utf8')
     while (timer < count) {
         timer++
-        /**
-         * debug
-         */
-        core.info(`timer${timer}`)
-
         data = data.replace(past_data, new_data)
     }
 
     try {
-        /**
-         * debug
-         */
-        core.info(data)
         fs.writeFileSync(file, data, 'utf8')
         core.info('File overwritten')
     } catch (e) {
